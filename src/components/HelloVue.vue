4166b328f8daf90e4a280af3df686c0789ae7a24
younkyo <inyounkyo@hanmail.net>
2022-12-20 17:29:54 +0900

    zz

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,20 @@
+<template>
+{{message}}
+</template>
+
+<script>
+export default {
+  name: 'HelloWVue',
+
+  data(){
+    return {
+      message: ' 안녕 Vue!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
