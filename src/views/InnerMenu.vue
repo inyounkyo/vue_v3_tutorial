b0d09609e0b0f9eebafa310a52b866da0dac59a5
inyounkyo <inyounkyo@hanmail.net>
2023-02-09 10:15:22 +0900

    vue_v3_tutorial InnerMenu

---
+++ src/views/InnerMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  inner menu  dd
+</template>
+<script>
+
+  export default {
+    name: 'InnerMenu',
+
+    data() {
+      return {
+
+      }
+    },
+    mounted() {
+    },
+    watch:{
+    },
+    methods:{
+
+    }
+
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
