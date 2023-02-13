ab8d564168f8f315706d547c8d92b49d4c060317
inyounkyo <inyounkyo@hanmail.net>
2023-02-13 10:22:58 +0900

    vue_v3_tutorial InnerMenu

--- src/router/LazyLoad.vue
+++ src/router/LazyLoad.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>
     use Router of LazyLoad.......ing
+    {{user}}
   </h1>
 </template>
 
@@ -10,9 +11,38 @@ export default {
 
   data(){
     return {
+      user:''
+    }
+  },
+  mounted() {
+    // fetch('http://localhost:8080/api/user/2')
+    //     .then((response) => response.json())
+    //     .then((data) => this.loadFn(data))
+    //     .then((data) => this.user = data);
 
+    this.postFn();
+  },
+  methods: {
+    loadFn(data){
+      console.log(typeof data);
+      console.log('loadFn', data)
+      console.log(`loadFn() ${data}`);
+    },
+    postFn(){
+      fetch("http://localhost:8080/api/user/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "Test",
+          body: "I am testing!",
+          userId: 1,
+        }),
+      })
     }
   }
+
 }
 </script>
 
